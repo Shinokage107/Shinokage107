0c6324333736a2b4bb2651c8290a7e70d297e74a
Alexander Braun <alexanderbraun121@gmail.com>
2022-10-29 18:56:14 +0200

    Implemented Puppeteer and fixed minor issues

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
-// index.js
-const Mustache = require('mustache');
-const fs = require('fs');
-const MUSTACHE_MAIN_DIR = './main.mustache';
-/**
-  * DATA is the object that contains all
-  * the data to be provided to Mustache
-  * Notice the "name" and "date" property.
-*/
+const Mustache = require("mustache");
+const fs = require("fs");
+const MUSTACHE_MAIN_DIR = "./main.mustache";
+const puppeteerService = require("./services/puppeteer.service");
+
 let DATA = {
-  name: 'Alexander',
-  date: new Date().toLocaleDateString('en-GB', {
-    weekday: 'long',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    timeZoneName: 'short',
-    timeZone: 'Europe/Stockholm',
+  name: "Alexander",
+  date: new Date().toLocaleDateString("en-GB", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    timeZoneName: "short",
+    timeZone: "Europe/Stockholm",
   }),
 };
-/**
-  * A - We open 'main.mustache'
-  * B - We ask Mustache to render our file with the data
-  * C - We create a README.md file with the generated output
-  */
+
 function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
     if (err) throw err;
     const output = Mustache.render(data.toString(), DATA);
-    fs.writeFileSync('README.md', output);
+    fs.writeFileSync("README.md", output);
   });
 }
-generateReadMe();
+
+async function setInstagramPosts() {
+  const instagramImages =
+    await puppeteerService.getLatestInstagramPostsFromAccount(
+      "visitstockholm",
+      3
+    );
+  DATA.img1 = instagramImages[0];
+  DATA.img2 = instagramImages[1];
+  DATA.img3 = instagramImages[2];
+}
+
+async function action() {
+  await setInstagramPosts();
+  generateReadMe();
+
+  await puppeteerService.close();
+}
